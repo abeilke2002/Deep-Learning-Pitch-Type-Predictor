16d010965b1613c4ebf43325bdf561500d076dcc
Aidan Beilke <130398965+abeilke2002@users.noreply.github.com>
2024-04-23 12:48:48 -0500

    Update index.js

--- Deploy/index.js
+++ Deploy/index.js
@@ -1,33 +1,30 @@
 async function runExample() {
+    try {
+        var x = [];
+        x[0] = parseFloat(document.getElementById('box1').value);
+        x[1] = parseFloat(document.getElementById('box2').value);
+        x[2] = parseFloat(document.getElementById('box3').value);
+        x[3] = parseFloat(document.getElementById('box4').value);
+        x[4] = parseFloat(document.getElementById('box5').value);
 
-    var x = new Float32Array( 1, 5 )
+        let tensorX = new onnx.Tensor(new Float32Array(x), 'float32', [1, 5]);
 
-    var x = [];
+        let session = new onnx.InferenceSession();
+        await session.loadModel("./Pitch_Type_Model_2.onnx");
+        let outputMap = await session.run({ input: tensorX });
+        let outputData = outputMap.get('output1');
 
-     x[0] = document.getElementById('box1').value;
-     x[1] = document.getElementById('box2').value;
-     x[2] = document.getElementById('box3').value;
-     x[3] = document.getElementById('box4').value;
-     x[4] = document.getElementById('box5').value;
+        console.log('Output Data:', outputData.data);
 
-    let tensorX = new onnx.Tensor(x, 'float32', [1, 5]);
-
-    let session = new onnx.InferenceSession();
-
-    await session.loadModel("./Pitch_Type_Model_2.onnx");
-    let outputMap = await session.run([tensorX]);
-    let outputData = outputMap.get('output1');
-
-   let predictions = document.getElementById('predictions');
-
-  predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
-   <table>
-     <tr>
-       <td>  Pitch Type Prediction  </td>
-       <td id="td0"> ${Math.round(outputData.data[0])} </td>
-     </tr>
-  </table>`;
-    
-
-
+        let predictions = document.getElementById('predictions');
+        predictions.innerHTML = `<hr> Got an output tensor with values: <br/>
+        <table>
+            <tr>
+                <td>Pitch Type Prediction</td>
+                <td id="td0">${Math.round(outputData.data[0])}</td>
+            </tr>
+        </table>`;
+    } catch (error) {
+        console.error('Error running model:', error);
+    }
 }
