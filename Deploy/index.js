7e71cb76cc76a0b2bfbd6fd71c111ad44bdde6d1
Aidan Beilke <130398965+abeilke2002@users.noreply.github.com>
2024-04-23 15:32:10 -0500

    Update index.js

--- Deploy/index.js
+++ Deploy/index.js
@@ -1,33 +1,32 @@
 async function runExample() {
+    // Create an array of float numbers from the input fields
+    var x = [
+        parseFloat(document.getElementById('box1').value),
+        parseFloat(document.getElementById('box2').value),
+        parseFloat(document.getElementById('box3').value),
+        parseFloat(document.getElementById('box4').value),
+        parseFloat(document.getElementById('box5').value),
+    ];
 
-    var x = new Float32Array( 1, 5 )
+    // Create a tensor from the array
+    let tensorX = new onnx.Tensor(new Float32Array(x), 'float32', [1, 5]);
 
-    var x = [];
-
-     x[0] = document.getElementById('box1').value;
-     x[1] = document.getElementById('box2').value;
-     x[2] = document.getElementById('box3').value;
-     x[3] = document.getElementById('box4').value;
-     x[4] = document.getElementById('box5').value;
-
-    let tensorX = new onnx.Tensor(x, 'float32', [1, 5]);
-
+    // Create a new inference session and load the model
     let session = new onnx.InferenceSession();
-
     await session.loadModel("./Pitch_Type_Model.onnx");
-    let outputMap = await session.run([tensorX]);
+
+    // Run the model
+    let outputMap = await session.run({input: tensorX});
     let outputData = outputMap.get('output1');
 
-   let predictions = document.getElementById('predictions');
-
-  predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
-   <table>
-     <tr>
-       <td>  Pitch Type Prediction  </td>
-       <td id="td0">  ${outputData.data[0].toFixed(2)}  </td>
-     </tr>
-  </table>`;
-    
-
-
+    // Update the predictions div
+    let predictions = document.getElementById('predictions');
+    predictions.innerHTML = `
+    <hr> Got an output tensor with values: <br/>
+    <table>
+        <tr>
+            <td> Pitch Type Prediction </td>
+            <td id="td0"> ${outputData.data[0].toFixed(2)} </td>
+        </tr>
+    </table>`;
 }
