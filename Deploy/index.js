b15cbe3aed1f2fffa5ef877af797b21b55f0c0d3
Aidan Beilke <130398965+abeilke2002@users.noreply.github.com>
2024-04-23 13:39:58 -0500

    Update index.js

--- Deploy/index.js
+++ Deploy/index.js
@@ -1,30 +1,33 @@
 async function runExample() {
-    try {
-        var x = [];
-        x[0] = parseFloat(document.getElementById('box1').value);
-        x[1] = parseFloat(document.getElementById('box2').value);
-        x[2] = parseFloat(document.getElementById('box3').value);
-        x[3] = parseFloat(document.getElementById('box4').value);
-        x[4] = parseFloat(document.getElementById('box5').value);
 
-        let tensorX = new onnx.Tensor(new Float32Array(x), 'float32', [1, 5]);
+    var x = new Float32Array( 1, 5 )
 
-        let session = new onnx.InferenceSession();
-        await session.loadModel("./Pitch_Type_Model_2.onnx");
-        let outputMap = await session.run({ input: tensorX });
-        let outputData = outputMap.get('output1');
+    var x = [];
 
-        console.log('Output Data:', outputData.data);
+     x[0] = document.getElementById('box1').value;
+     x[1] = document.getElementById('box2').value;
+     x[2] = document.getElementById('box3').value;
+     x[3] = document.getElementById('box4').value;
+     x[4] = document.getElementById('box5').value;
 
-        let predictions = document.getElementById('predictions');
-        predictions.innerHTML = `<hr> Got an output tensor with values: <br/>
-        <table>
-            <tr>
-                <td>Pitch Type Prediction</td>
-                <td id="td0">${Math.round(outputData.data[0])}</td>
-            </tr>
-        </table>`;
-    } catch (error) {
-        console.error('Error running model:', error);
-    }
+    let tensorX = new onnx.Tensor(x, 'float32', [1, 5]);
+
+    let session = new onnx.InferenceSession();
+
+    await session.loadModel("./Pitch_Type_Model.onnx");
+    let outputMap = await session.run([tensorX]);
+    let outputData = outputMap.get('output1');
+
+   let predictions = document.getElementById('predictions');
+
+  predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
+   <table>
+     <tr>
+       <td>  Pitch Type Prediction  </td>
+       <td id="td0">  ${outputData.data[0].toFixed(2)}  </td>
+     </tr>
+  </table>`;
+    
+
+
 }
